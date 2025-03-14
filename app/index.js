06920472ab73f1c2e18eaeab13c5d684f15f475d
MatiasGLR <89618405+MatiasGLR@users.noreply.github.com>
2025-03-14 05:43:46 +0000

    a

--- app/index.js
+++ app/index.js
@@ -60,7 +60,7 @@ app.use(cookieParser());
 // Base de datos
 const db = new sqlite.Database(
     //path.resolve(__dirname, './files/db/enforth.db'),
-    path.resolve('/home/codespace/sqlite-db/mi_base_de_datos.db'),
+    path.resolve('/home/codespace/sqlite-db/enforth.db'),
     (error) => {
         if(error) {
             return console.error(error);
