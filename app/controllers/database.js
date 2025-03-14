99ade51054d71f02840c1fa26e2a3ab2d29bd7c0
MatiasGLR <89618405+MatiasGLR@users.noreply.github.com>
2025-03-14 05:33:38 +0000

    a

--- app/controllers/database.js
+++ app/controllers/database.js
@@ -4,7 +4,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const db = new sqlite3.Database(path.resolve(__dirname, '../files/db/enforth.db'), (err) => {
+//const db = new sqlite3.Database(path.resolve(__dirname, '../files/db/enforth.db'), (err) => {
+const db = new sqlite3.Database('/home/codespace/sqlite-db/mi_base_de_datos.db', (err) => {
     if (err) {
         console.error("Error al conectar con la base de datos:", err.message);
     } else {
--- app/index.js
+++ app/index.js
@@ -59,7 +59,8 @@ app.use(cookieParser());
 
 // Base de datos
 const db = new sqlite.Database(
-    path.resolve(__dirname, './files/db/enforth.db'),
+    //path.resolve(__dirname, './files/db/enforth.db'),
+    path.resolve('/home/codespace/sqlite-db/mi_base_de_datos.db'),
     (error) => {
         if(error) {
             return console.error(error);
